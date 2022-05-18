cc08ffefe8895e95fc1bae3f11fa969c34514f19
kessef <dkdue@hotmail.com>
2022-05-18 21:45:21 +0200

    Beta Test
    First functional test

---
+++ VRising.cs/VRising.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Query;
+using WindowsGSM.GameServer.Engine;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+
+
+namespace WindowsGSM.Plugins
+{
+    public class VRising : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.VRising", // WindowsGSM.XXXX
+            author = "kessef",
+            description = "WindowsGSM plugin for supporting VRising Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/dkdue/WindowsGSM.VRising", // Github repository link (Best practice)
+            color = "#34c9eb" // Color Hex
+        };
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "1829350"; // Game server appId
+
+        // - Standard Constructor and properties
+        public VRising(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+
+        // - Game server Fixed variables
+        public override string StartPath => @"VRisingServer.exe"; // Game server start path
+        public string FullName = "VRising Dedicated Server"; // Game server FullName
+        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
+        public int PortIncrements = 10; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        // - Game server default values
+        public string Port = "9876"; // Default port
+        public string QueryPort = "9877"; // Default query port
+        public string Defaultmap = "world1"; // Default map name
+        public string Maxplayers = "40"; // Default maxplayers
+        public string Additional = ""; // Additional server start parameter
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            //No config file seems
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+			
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }			
+			
+		
+
+            // Prepare start parameter
+
+			string param = $"-batchmode -nographics {_serverData.ServerParam}" + (!AllowsEmbedConsole ? " -log" : string.Empty);	
+	
+
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = param,
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+
+                // Start Process
+                try
+                {
+                    p.Start();
+                }
+                catch (Exception e)
+                {
+                    Error = e.Message;
+                    return null; // return null if fail to start
+                }
+
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                return p;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+
+// - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                 Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                 Functions.ServerConsole.SendWaitToMainWindow("^c");
+            });
+			 await Task.Delay(20000);
+        }
+
+    }
+}
